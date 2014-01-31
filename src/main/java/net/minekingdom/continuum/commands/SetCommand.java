f18daf622e6feca1c3be6b960b3c444cb322c23b
Flyffmario <flyffmario@laposte.net>
2014-01-31 20:04:54 +0100

    Fixed SetCommand

--- src/main/java/net/minekingdom/continuum/commands/SetCommand.java
+++ src/main/java/net/minekingdom/continuum/commands/SetCommand.java
@@ -9,55 +9,70 @@ import org.bukkit.ChatColor;
 import org.bukkit.Difficulty;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SetCommand {
 	
 	@SubCommand(name = "set", min = 2, max = 3, permission = "continuum.set")
 	public void set(CommandSender sender, String[] args) {
 		
-		if (args[2] == null) {
-			throw new CommandException("No destination specified !");
-		}
+		Dimension dim;
 		
-		String[] name = args[2].split(":");
-		
-		Universe world = Continuum.getInstance().getWorldManager().getWorld(name[0]);
-		
-		if (world == null) {
-			throw new CommandException("The world \"" + name[0] + "\" does not exist.");
-		}
-		
-		Dimension dim = world.getDimension(name[1]);
-		
-		if (dim == null) {
-			throw new CommandException("The dimension \"" + name[1] + "\" does not exist.");
+		if (args.length == 2) {
+			
+			if (!(sender instanceof Player)) {
+				throw new CommandException("You must specify a dimension");
+			}
+			
+			dim = Dimension.get(((Player) sender).getWorld());
+			
+			if (dim == null) {
+				throw new CommandException("The requested dimension is not a valid Continuum dimension.");
+			}
+			
+		} else {
+			
+			String[] name = args[2].split(":");
+			
+			Universe world = Continuum.getInstance().getWorldManager().getWorld(name[0]);
+			
+			if (world == null) {
+				throw new CommandException("The world \"" + name[0] + "\" does not exist.");
+			}
+			
+			dim = world.getDimension(name[1]);
+			
+			if (dim == null) {
+				throw new CommandException("The dimension \"" + name[1] + "\" does not exist.");
+			}
+			
 		}
 		
 		switch (args[0]) {
-			
-			case "animals":
-				dim.setAnimals(Boolean.parseBoolean(args[1]));
-				break;
-			
-			case "difficulty":
-				dim.setDifficulty(Difficulty.valueOf(args[1]));
-				break;
-			
-			case "gamemode":
-				sender.sendMessage(ChatColor.RED + "This command does not already exist.");
-				break;
-			
-			case "monsters":
-				dim.setMonsters(Boolean.parseBoolean(args[1]));
-				break;
-			
-			case "pvp":
-				dim.setPVP(Boolean.parseBoolean(args[1]));
-				break;
-			
-			case "weather":
-				sender.sendMessage(ChatColor.RED + "This command does not already exist.");
-				break;
+		
+		case "animals":
+			dim.setAnimals(Boolean.parseBoolean(args[1]));
+			break;
+		
+		case "difficulty":
+			dim.setDifficulty(Difficulty.valueOf(args[1]));
+			break;
+		
+		case "gamemode":
+			sender.sendMessage(ChatColor.RED + "This command does not exist yet.");
+			break;
+		
+		case "monsters":
+			dim.setMonsters(Boolean.parseBoolean(args[1]));
+			break;
+		
+		case "pvp":
+			dim.setPVP(Boolean.parseBoolean(args[1]));
+			break;
+		
+		case "weather":
+			sender.sendMessage(ChatColor.RED + "This command does not exist yet.");
+			break;
 		}
 	}
 }
